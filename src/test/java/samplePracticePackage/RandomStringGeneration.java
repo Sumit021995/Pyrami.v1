da19eeb72c17f519b5648a3375dcf028aa8d90f0
Sumit021995 <kaku.saurav@gmail.com>
2025-04-04 10:38:15 +0530

    new commit

--- src/test/java/samplePracticePackage/RandomStringGeneration.java
+++ src/test/java/samplePracticePackage/RandomStringGeneration.java
@@ -3,32 +3,34 @@ package samplePracticePackage;
 import java.util.Random;
 
 public class RandomStringGeneration {
-	public String getRandomString(int stringLength)
+	public StringBuilder getRandomString(int stringLength)
 	{
-		String randomString="";
+//		String randomString="";
+		StringBuilder sb = new StringBuilder(stringLength);
 		String alfabets = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
 		for(int i=0;i<stringLength;i++)
 		{
 			int randomNumber = new Random().nextInt(alfabets.length());
-			randomString += alfabets.charAt(randomNumber);
+			sb.append(alfabets.charAt(randomNumber));
 		}
-		return randomString;
+		return sb;
 	}
-	public String getRandomAlfaNumericString(int stringLength)
+	public StringBuilder getRandomAlfaNumericString(int stringLength)
 	{
-		String randomString="";
+		StringBuilder sb = new StringBuilder(stringLength);
+//		String randomString="";
 		String alfabets = "QWERTYUIOPASDFGHJKLZ0123456789XCVBNMqwertyuiopasdfghjklzxcvbnm";
 		for(int i=0;i<stringLength;i++)
 		{
 			int randomNumber = new Random().nextInt(alfabets.length());
-			randomString += alfabets.charAt(randomNumber);
+			sb.append(alfabets.charAt(randomNumber));
 		}
-		return randomString;
+		return sb;
 	}
 	public static void main(String[] args) {
-		String randomString = new RandomStringGeneration().getRandomString(12);
+		StringBuilder randomString = new RandomStringGeneration().getRandomString(12);
 		System.out.println(randomString);
-		String randomAlfaNumericString = new RandomStringGeneration().getRandomAlfaNumericString(12);
+		StringBuilder randomAlfaNumericString = new RandomStringGeneration().getRandomAlfaNumericString(100);
 		System.out.println(randomAlfaNumericString);
 	}
 }
