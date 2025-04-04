5b46d27130029e78db0e8ee7e4a55665c81be145
Sumit021995 <kaku.saurav@gmail.com>
2025-04-04 10:25:49 +0530

    new commit

--- src/main/java/genericUtility/IPathUtility.java
+++ src/main/java/genericUtility/IPathUtility.java
@@ -3,4 +3,5 @@ package genericUtility;
 public interface IPathUtility {
 	final static String propertiesFile = ".\\src\\test\\resources\\TestData\\vtigerCommonData.properties";
 	final static String excelFile = ".\\src\\test\\resources\\TestData\\TestExcel.xlsx";
+	final static String HrEmailsExcel = ".\\src\\test\\resources\\TestData\\HrEmailed.xlsx";
 }
