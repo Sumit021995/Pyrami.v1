baa31f7addb48eec9bbbc1d62d78b5fe7027c9e9
Sumit021995 <kaku.saurav@gmail.com>
2025-04-05 10:55:50 +0530

    new commit

---
+++ src/test/java/samplePracticePackage/FetchingDataFromJson.java
@@ -0,0 +1,28 @@
+package samplePracticePackage;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.json.JSONObject;
+
+import genericUtility.IPathUtility;
+
+public class FetchingDataFromJson {
+	public static void main(String[] args) throws Exception {
+		FetchingDataFromJson data = new FetchingDataFromJson();
+		// BY jsonReader
+		System.out.println(data.jsonReader("browser"));
+		System.out.println(data.jsonReader("url"));
+		System.out.println(data.jsonReader("username"));
+		System.out.println(data.jsonReader("password"));
+		System.out.println("***************");
+		
+	}
+	public String jsonReader(String key) throws Exception
+	{
+		String content = new String(Files.readAllBytes(Paths.get(IPathUtility.jsonFilePath)));
+		JSONObject jsonObj = new JSONObject(content);
+		return jsonObj.getString(key);
+	}
+	
+}
