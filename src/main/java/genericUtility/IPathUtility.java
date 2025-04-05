667d79e0cdb874ea3379afa4ae5d4b496b8fe5c3
Sumit021995 <kaku.saurav@gmail.com>
2025-04-05 11:03:30 +0530

    new Commit

--- src/main/java/genericUtility/IPathUtility.java
+++ src/main/java/genericUtility/IPathUtility.java
@@ -4,4 +4,5 @@ public interface IPathUtility {
 	final static String propertiesFile = ".\\src\\test\\resources\\TestData\\vtigerCommonData.properties";
 	final static String excelFile = ".\\src\\test\\resources\\TestData\\TestExcel.xlsx";
 	final static String HrEmailsExcel = ".\\src\\test\\resources\\TestData\\HrEmailed.xlsx";
+	static final String jsonFilePath = ".\\src\\test\\resources\\TestData\\preCondition.json";
 }
